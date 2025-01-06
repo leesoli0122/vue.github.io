553540db3620b484997905db060625fdc7e150fc
leesoli0122 <leesoli0122@gmail.com>
2025-01-06 09:18:03 +0900

    [vue 배우는 중]

---
+++ vue-cli/src/a.vue
@@ -0,0 +1,29 @@
+<!-- 
+    var appHeader = {
+        template: <div>header</div>,
+        methods: {
+            addNum: function() {
+            
+            }
+        }
+    }
+-->
+<template>
+    <!-- html -->
+    <div>header</div>
+</template>
+
+<script>
+export default {
+    //javascript
+    methods: {
+        addNum: function() {
+
+        }
+    }
+}
+</script>
+
+<style>
+    /* css */
+</style>
