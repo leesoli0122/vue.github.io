9d8d22f1a7af904d33735a2432ebf678a6397c7f
leesoli0122 <leesoli0122@gmail.com>
2025-01-08 15:09:27 +0900

    [app.vue에서 오류생김]

---
+++ Gymcoding/src/components/AppView.vue
@@ -0,0 +1,16 @@
+<template>
+    <main>
+        <book-component></book-component>
+        <book-component></book-component>
+        <book-component></book-component>
+        <book-component></book-component>
+        <book-component></book-component>
+    </main>
+</template>
+<script>
+export default {}
+</script>
+<style>
+
+</style>
+
---
+++ Gymcoding/src/components/BookComponent.vue
@@ -0,0 +1,12 @@
+<template>
+    <article>
+        <div class="subtitle">제목</div>
+        <div class="title">VUE.JS 강좌</div>
+    </article>
+</template>
+<script>
+    export default {}
+</script>
+<style>
+
+</style>
