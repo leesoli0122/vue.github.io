f74b8082b1e42c92fdb77e75681969c5c49c6697
leesoli0122 <leesoli0122@gmail.com>
2025-01-08 15:09:48 +0900

    [app.vue 오류 왜인지는 모름]

--- Gymcoding/src/App.vue
+++ Gymcoding/src/App.vue
@@ -2,10 +2,11 @@
     <app-header></app-header>
     <app-nav></app-nav>
     <app-view></app-view>
-</template>
-<script>
-export default {}
-</script>
-<style>
-@import "./asset/base.css";
-</style>
+  </template>
+  <script>
+  export default {};
+  </script>
+  <style>
+  @import "./assets/base.css";
+  </style>
+  
