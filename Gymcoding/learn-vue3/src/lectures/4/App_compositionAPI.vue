fb3ca708f238e1fb5c8ee5a687aa9f200bf9b627
leesoli0122 <leesoli0122@gmail.com>
2025-01-10 17:56:31 +0900

    [setup 끝나고 template 들어갈갈 예정]

---
+++ Gymcoding/learn-vue3/src/lectures/4/App_compositionAPI.vue
@@ -0,0 +1,48 @@
+<template>
+	<div>
+		<h2>반응형 메시지</h2>
+		<p>{{ reactiveMessage }}</p>
+		<button v-on:click="addReactiveMessage">Add message</button>
+		<h2>일반 메시지</h2>
+		<p>{{ normalmessage }}</p>
+		<button v-on:click="addNormalMessage">Add message</button>
+	</div>
+</template>
+
+<script>
+import { isRef, onBeforeMount, onMounted, ref } from 'vue'; // 추가: ref를 Vue에서 가져옴
+
+export default {
+	setup() {
+		console.log('setup()');
+		const reactiveMessage = ref('hello reactive message');
+		const addReactiveMessage = () => {
+			reactiveMessage.value = reactiveMessage.value + '!';
+		};
+		console.log('isRef(reactiveMessage)', isRef(reactiveMessage));
+
+		let normalmessage = 'hello normal message';
+		const addNormalMessage = () => {
+			normalmessage = normalmessage + '!';
+		};
+		console.log('isRef(normalmessage)', isRef(normalmessage));
+
+		onMounted(() => {
+			console.log('onMounted');
+		});
+
+		onBeforeMount(() => {
+			console.log('onBeforeMount');
+		});
+
+		return {
+			reactiveMessage,
+			normalmessage,
+			addReactiveMessage,
+			addNormalMessage,
+		};
+	},
+};
+</script>
+
+<style></style>
--- Gymcoding/learn-vue3/src/main.js
+++ Gymcoding/learn-vue3/src/main.js
@@ -1,6 +1,14 @@
-import './assets/main.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { createApp } from 'vue';
+import App from './App.vue';
 
-import { createApp } from 'vue'
-import App from './App.vue'
+const app = createApp(App);
 
-createApp(App).mount('#app')
+// app.component('AppCard', AppCard);
+
+app.provide('app-message', 'app message 입니다');
+//
+app.config.globalProperties.msg = 'hello';
+app.provide('msg', 'hello msg');
+app.mount('#app');
+import 'bootstrap/dist/js/bootstrap.js';
