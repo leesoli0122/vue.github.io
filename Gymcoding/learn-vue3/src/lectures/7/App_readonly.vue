fc7243acea8c227b191050258e13c41bb5634e86
leesoli0122 <leesoli0122@gmail.com>
2025-01-17 14:13:21 +0900

    [7번째 파일 끝]

---
+++ Gymcoding/learn-vue3/src/lectures/7/App_readonly.vue
@@ -0,0 +1,23 @@
+<template>
+	<div></div>
+</template>
+
+<script>
+import { reactive, readonly } from 'vue';
+
+export default {
+	setup() {
+		const original = reactive({
+			count: 0,
+		});
+		const copy = readonly(original);
+		original.count++;
+		copy.count++;
+		console.log(original.count); // 1
+		console.log(copy.count); // 1
+		return {};
+	},
+};
+</script>
+
+<style></style>
---
+++ Gymcoding/learn-vue3/src/lectures/7/App_ref_object_array.vue
@@ -0,0 +1,30 @@
+<template>
+	<div></div>
+</template>
+
+<script>
+import { reactive, ref } from 'vue';
+
+export default {
+	setup() {
+		// ref -> Object
+		const count = ref(0);
+		const state = reactive({
+			count, //count: count 값이 같으면 생략 가능
+		});
+
+		count.value++;
+
+		//console.log(count.value); // 반응형으로 접근하기 위해서. 콘솔 창에서 값은 0으로 맞게 나온다.
+		// console.log('state.count: ', state.count.value); 반응형으로 접근하겠다는 생각으로 똑같이 value를 넣어줬지만 콘솔창에 값은 state.count:  undefined로 나온다.
+		//console.log('state.count: ', state.count); // 콘솔 창 값이 state.count:  0으로 맞게 나온다.
+		// ref -> Array
+		const message = ref('Hello');
+		const arr = reactive([message]);
+		console.log('arr[0]: ', arr[0].value);
+		return {};
+	},
+};
+</script>
+
+<style></style>
