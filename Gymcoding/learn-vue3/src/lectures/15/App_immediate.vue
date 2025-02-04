e63c0ce73c2e59b5198ccf7bcf9158fb541c8649
leesoli0122 <leesoli0122@gmail.com>
2025-02-04 11:18:07 +0900

    섹션 4 끝

---
+++ Gymcoding/learn-vue3/src/lectures/15/App_immediate.vue
@@ -0,0 +1,41 @@
+<template>
+	<div>
+		<p>{{ message }}</p>
+		<p>{{ reverseMessage }}</p>
+	</div>
+</template>
+
+<script>
+import { ref, watch } from 'vue';
+
+// computed : 반응형 데이터의 종속 관계를 자동으로 세팅하고자 할 때 ex) message에 따라서 어떠한 값이 세팅되어야 할 때
+// watch: 반응형 데이터가 변경되는 시점에 돔을 변경하던가 api를 호출해서 다른 비즈니스 로직을 수행할 때 이렇게 복잡한 로직을 수행할 때 watch를 사용하는게 더 효율적
+export default {
+	setup() {
+		const message = ref('hello vue3');
+		const reverseMessage = ref('');
+
+		// watch(
+		// 	message,
+		// 	newValue => {
+		// 		reverseMessage.value = newValue.split('').reverse().join('');
+		// 	},
+		// 	{
+		// 		immediate: true,
+		// 	},
+		// );
+
+		// 위에 watch와 아래 내용 같은 것
+
+		const reverseFunction = () => {
+			console.log('즉시실행함');
+			reverseMessage.value = message.value.split('').reverse().join('');
+		};
+		watch(message, reverseFunction);
+		reverseFunction();
+		return { message, reverseMessage };
+	},
+};
+</script>
+
+<style></style>
---
+++ Gymcoding/learn-vue3/src/lectures/15/App_watchEffect.vue
@@ -0,0 +1,42 @@
+<template>
+	<div>
+		<!-- form을 변경할 때마다 자동으로 저장하는 예제 -->
+		<!-- <form action="" @submit.prevent="save(title, contents)">
+			<input v-model.lazy="title" type="text" placeholder="title" />
+			<textarea v-model.lazy="contents" placeholder="contents"></textarea>
+			<hr />
+			<button>저장</button>
+		</form> -->
+		<form action="" @submit.prevent>
+			<input v-model.lazy="title" type="text" placeholder="title" />
+			<textarea v-model.lazy="contents" placeholder="contents"></textarea>
+			<hr />
+			<button @click="save(title, contents)">저장</button>
+		</form>
+	</div>
+</template>
+
+<script>
+import { ref, watchEffect } from 'vue';
+
+export default {
+	setup() {
+		const title = ref('');
+		const contents = ref('');
+
+		const save = (title, contents) => {
+			console.log(`저장되었습니다. title: ${title}, contents: ${contents}`);
+		};
+
+		// watchEffect는 watch와 다르게 기본적으로 최초 한 번 즉시 실행이다
+		watchEffect(() => {
+			console.log('watchEffect');
+			save(title.value, contents.value);
+		});
+
+		return { title, contents, save };
+	},
+};
+</script>
+
+<style></style>
